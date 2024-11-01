439e61cb33cbd3975d6ee0abb767a52b431ba9f9
Alfarizki Ramadhan <155267889+AlpaZZ@users.noreply.github.com>
2024-11-01 18:21:18 +0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -8,8 +8,6 @@
     hypr-contrib.url = "github:hyprwm/contrib";
     hyprpicker.url = "github:hyprwm/hyprpicker";
   
-    alejandra.url = "github:kamadorueda/alejandra/3.0.0";
-  
     hyprland = {
       type = "git";
       url = "https://github.com/hyprwm/Hyprland";
